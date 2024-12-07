046a1166da54cad1547665efdbff4d0ac5fff4c7
yshiraishiy <yuya0505soccer@gmail.com>
2024-12-07 14:13:21 +0900

    不要な読み込みファイルの削除

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { Todo } from "./Todo.jsx";
 
 createRoot(document.getElementById("root")).render(
